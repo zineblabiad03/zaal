f6f97cb0e53f3fc5fa57de33f13f09bd8823866c
Ange TOSSOU <angetossou.dev@gmail.com>
2025-01-19 14:49:12 +0100

    First access to Amadeus API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AmadeusService } from './amadeus.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'zaal';
+  flightOffers: any[] = [];
+
+  constructor(private amadeusService: AmadeusService) {}
+
+  ngOnInit(): void {
+    this.amadeusService.getAccessToken().subscribe({
+      next: (response) => {
+        console.log('Access Token:', response.access_token);
+        this.amadeusService.setToken(response.access_token);
+
+        this.amadeusService
+          .fetchFlightOffers('NYC', 'LAX', '2025-02-01')
+          .subscribe({
+            next: (data) => {
+              console.log('Flight Offers:', data);
+              this.flightOffers = data.data;
+            },
+            error: (error) => console.error('Error fetching flight offers:', error)
+          });
+      },
+      error: (error) => console.error('Error retrieving access token:', error)
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"zaal-d2b99","appId":"1:517938653680:web:854cd14199e6a9d814d535","storageBucket":"zaal-d2b99.firebasestorage.app","apiKey":"AIzaSyAOesZDJdieu-fKxDpDe2bn4ydWKX4rn9M","authDomain":"zaal-d2b99.firebaseapp.com","messagingSenderId":"517938653680","measurementId":"G-YZBMCEL0C5"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
