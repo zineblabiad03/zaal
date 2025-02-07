ccc2c201b0b512480c730893d44ec1bd4674df7a
Ange TOSSOU <angetossou.dev@gmail.com>
2025-02-07 11:13:27 +0100

    Retrieve good versions of package

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: 'AIzaSyAOesZDJdieu-fKxDpDe2bn4ydWKX4rn9M',
+      authDomain: 'YOUR_AUTH_DOMAIN',
+      databaseURL: 'YOUR_DATABASE_URL',
+      projectId: 'zaal-d2b99',
+      storageBucket: 'YOUR_STORAGE_BUCKET',
+      messagingSenderId: 'YOUR_MESSAGING_SENDER_ID'
+    }
+  };
